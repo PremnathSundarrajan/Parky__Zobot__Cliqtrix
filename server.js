1118d939ca91174d1ce5416151684af0780a21bc
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-11-26 14:50:51 +0530

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const bot_Router = require("./router/bot_Router");
+
+app.use(express.json());
+
+app.use("/api", bot_Router);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
