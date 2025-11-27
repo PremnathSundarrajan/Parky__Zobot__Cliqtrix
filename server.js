bdf9525541e773712298ffe9ee3d497862ae23a5
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-11-27 11:30:00 +0530

    migration fix

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(sessionMiddleware); 
 console.log(process.env.DATABASE_URL);
+app.get("/",(req,res)=>{
+
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 app.post("/signup", async (req, res) => {
   try {
     const { name, email, phone, password } = req.body;
