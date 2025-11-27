7fe4cc2c5cbc6408f0fbb9d30ef6c128eed97aae
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-11-27 12:46:24 +0530

    jwt added

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,13 @@
 // }
 
 function isAuthenticated(req, res, next) {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers['authorization'];
   if (!authHeader) return res.status(401).json({ message: "No token provided" });
 
   const token = authHeader.split(" ")[1]; // Bearer <token>
   if (!token) return res.status(401).json({ message: "Invalid token format" });
 
-  jwt.verify(token, SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.SESSION_SECRET, (err, decoded) => {
     if (err) return res.status(403).json({ message: "Invalid or expired token" });
 
     req.user = decoded; // attach user info to request
