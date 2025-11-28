40c9941cacba3483214e789e1df79cf239059d92
PremnathSundarrajan <premsundarrajan4@gmail.com>
2025-11-28 22:03:27 +0530

    visitor id stored in table

--- controller/bot_Controller.js
+++ controller/bot_Controller.js
@@ -20,11 +20,16 @@ const bot_explore =  (req, res) => {
 };
 
 const bot_explore_area = (req, res) => {
-  
-  const body = req.user.body;
+  try{
+     const body = req.user.body;
   console.log(body);
   //city name varum, then we need to suggests parking areas of chennai here
   res.status(200).send(`${body.place} has 20 parking areas`);
+  }catch(err){
+    console.log(err);
+    res.status(500).send("Internal Server");  
+  }
+ 
 };
 
 const bot_book =  (req, res) => {
